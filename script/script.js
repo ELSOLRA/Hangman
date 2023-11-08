16cdff3d4125f4a22ef6857e7e0c14e031cb4888
linnjoha <linn.wallin@hotmail.com>
2023-11-08 19:08:08 +0100

    foreach innerhtml

--- script/script.js
+++ script/script.js
@@ -1,61 +1,57 @@
-import { wordList } from './wordsList.js';
-
-
+import { wordList } from "./wordsList.js";
 
 let wrongLetter = [];
 let correctLetter = [];
-let wrongGuessingCounter= 6;
-let randomWord='';
-let setRandomWord= function (listOfWords) {
- randomWord= listOfWords[ Math.floor(Math.random() * (listOfWords.length))]
- randomWord = randomWord.toUpperCase();
- randomWord=randomWord.split('');
-}
+let wrongGuessingCounter = 6;
+let randomWord = "";
+let setRandomWord = function (listOfWords) {
+  randomWord = listOfWords[Math.floor(Math.random() * listOfWords.length)];
+  randomWord = randomWord.toUpperCase();
+  randomWord = randomWord.split("");
+};
 setRandomWord(wordList);
 
-
-let buttons = document.querySelectorAll('.keyboard__button');
+let buttons = document.querySelectorAll(".keyboard__button");
 let gameOver = false;
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    if(!gameOver) {
-    handlGuess(button.innerHTML.toUpperCase());
-    button.disabled = true;
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (!gameOver) {
+      handlGuess(button.innerHTML.toUpperCase());
+      button.disabled = true;
     }
-  })
- 
-})
-    function handlGuess (guess) {
-        if (randomWord.includes(guess) && wrongGuessingCounter > 0 ) {
-            console.log(`${guess}`);
-            correctLetter.push(guess);
-          } else if (!randomWord.includes(guess) && wrongGuessingCounter > 0){
-            console.log(`${guess} - incorrect`);
-            wrongLetter.push(guess);
-            wrongGuessingCounter--;
-            console.log(wrongGuessingCounter);
-          } else {
-            gameOver = true;
-            disableAllButtons();
-            }
-    }
-    
-    console.log(correctLetter);
+  });
+});
+function handlGuess(guess) {
+  if (randomWord.includes(guess) && wrongGuessingCounter > 0) {
+    console.log(`${guess}`);
+    correctLetter.push(guess);
+  } else if (!randomWord.includes(guess) && wrongGuessingCounter > 0) {
+    console.log(`${guess} - incorrect`);
+    wrongLetter.push(guess);
+    wrongGuessingCounter--;
+    console.log(wrongGuessingCounter);
+  } else {
+    gameOver = true;
+    disableAllButtons();
+  }
+}
 
-    
-document.addEventListener('keydown', event => {
-    if(!gameOver) {
+console.log(correctLetter);
+
+document.addEventListener("keydown", (event) => {
+  if (!gameOver) {
     handlGuess(event.key.toUpperCase());
-    }
-})
+  }
+});
 
 function disableAllButtons() {
-    buttons.forEach(button => {
-      button.disabled = true;
-    });
-};
+  buttons.forEach((button) => {
+    button.disabled = true;
+  });
+}
 
-let placeHolder = document.getElementById('word-placeholder');
+let placeHolder = document.getElementById("word-placeholder");
+/*
 let wordLength = randomWord.length;
 let blanksLetters = []
 for (var i = 0; i < wordLength; i++) { 
@@ -63,6 +59,8 @@ for (var i = 0; i < wordLength; i++) {
     }
     console.log(blanksLetters);
     placeHolder.innerText = blanksLetters.join("");
-/* randomWord.forEach(index => {
-    placeHolder.innerHTML = "_";
-} )*/
+*/
+
+randomWord.forEach((letter) => {
+  placeHolder.innerHTML = placeHolder.innerHTML + "_";
+});
